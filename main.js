ac1f874aca5a318e48357b403a371517495ab3d5
Dimas Triana <92770131+dimstry@users.noreply.github.com>
2022-06-19 16:21:24 +0700

    memperbaiki sintaks js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
+const x = document.getElementById("myAudio");
 // saat tombol play fi click
 $('#play_button').on('click', function() {
   $('#play_button').css("display", "none");
   $('#pause_button').css("display", "block");
-  var x = document.getElementById("myAudio");
   x.play();
 });
 // saat tombol pause di click
 $('#pause_button').on('click', function() {
   $('#pause_button').css("display", "none");
   $('#play_button').css("display", "block");
-  var x = document.getElementById("myAudio");
   x.pause();
 });
